1dc36f7c0e0ee0f99fd1f2244ceee2a2ca974e91
kostyazu1985-arch <kostyazu1985@gmail.com>
2026-08-22 14:47:00 +0300

    project

--- first/password_generator_project/password/views.py
+++ first/password_generator_project/password/views.py
@@ -15,6 +15,12 @@ def password(request):
     if request.GET.get('uppercase'):
         char.extend([chr(i) for i in range(65, 91)])
 
+    if request.GET.get('numbers'):
+        char.extend([chr(i) for i in range(48, 58)])
+
+    if request.GET.get('special'):
+        char.extend([chr(i) for i in range(33, 48)])
+
     length = int(request.GET.get('length'))
     psw = ''
     for i in range(length):
