123ee8181f812a5af154a2af897b97b02919a5fd
kostyazu1985-arch <kostyazu1985@gmail.com>
2026-08-26 18:09:45 +0300

    project

--- second/personal_portfolio/blog/views.py
+++ second/personal_portfolio/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+def blogs(request):
+    blog = Blog.objects.order_by('-data')
+    return render(request, 'blog/blogs.html', {'blogs': blog})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/details.html', {'blog': blog})
